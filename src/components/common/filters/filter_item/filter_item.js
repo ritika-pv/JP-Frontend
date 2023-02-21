d3befcec793ebc9750c18f023bcf75a4a30d7303
Vikas <vikash.agarawal2012@gmail.com>
2023-02-21 16:25:07 +0530

    footer done

--- src/components/common/filters/filter_item/filter_item.js
+++ src/components/common/filters/filter_item/filter_item.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./filter_item.css";
 
 const FilterItem = ({ filter }) => {
-  return <div className="filter-item">{filter.icon && filter.icon}
+  return <div className="filter-item cur-pointer">{filter.icon && filter.icon}
   <div className="filter-name">{filter.title}</div>
   
   </div>;
--- src/components/common/footer/footer.js
+++ src/components/common/footer/footer.js
@@ -1,7 +1,110 @@
-import React from 'react'
+import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import "./footer.css";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <MDBFooter className="text-center" color="white" bgColor="dark">
+      <MDBContainer className="p-4">
+        <section className="mb-4">
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="facebook-f" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="twitter" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="google" />
+          </MDBBtn>
+
+          <MDBBtn
+            outline
+            color="light"
+            floating
+            className="m-1"
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="instagram" />
+          </MDBBtn>
+        </section>
+
+        <section className="">
+          <form action="">
+            <MDBRow className="d-flex justify-content-center">
+              <MDBCol size="auto">
+                <p className="pt-2">
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </MDBCol>
+
+              <MDBCol md="5" start>
+                <MDBInput
+                  contrast
+                  type="email"
+                  label="Email address"
+                  className="mb-4"
+                />
+              </MDBCol>
+
+              <MDBCol size="auto">
+                <MDBBtn outline color="light" type="submit" className="mb-4">
+                  Subscribe
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </form>
+        </section>
+
+        <section className="mb-4">
+          <p>
+            Food Panda is an an Indian multinational restaurant aggregator and
+            food delivery company.It serves around 20+ countries and covers
+            5000+ cities. It is the world's largest food delivery company.It serves Indian,Itanlian,Chinese,Thai and many more dishes.
+          </p>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+      >
+        © 2023 Copyright: 
+        <a className="text-white" href="#!">
+          FoodPanda.Inc
+        </a>
+      </div>
+    </MDBFooter>
+  );
+};
