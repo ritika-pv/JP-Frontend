5eb3a5e7ce5092f2e0c251dcaee384ae3a3ee6c9
Vikas <vikash.agarawal2012@gmail.com>
2023-02-22 12:38:06 +0530

    homepage done

--- src/components/common/explore_section/explore_card/explore_card.js
+++ src/components/common/explore_section/explore_card/explore_card.js
@@ -10,11 +10,31 @@ const ExploreCard = ({ restaurant }) => {
   const cuisines = restaurant?.info?.cuisine
     ?.map((item) => item.name)
     .slice(0, 3);
+  const offers = restaurant?.bulkOffers ?? [];
+  const discount =
+    offers.length > 1
+      ? offers[1].text
+      : offers.length === 1
+      ? offers[0].text
+      : null;
   return (
     <div className="explore-card cur-pointer">
       <div className="explore-card-cover">
         <img src={coverImg} alt={name} className="explore-card-image" />
         <div className="delivery-time">{deliveryTime}</div>
+        {discount && <div className="discount absolute-center">{discount}</div>}
+      </div>
+      <div className="res-row">
+        <div className="res-name">{name}</div>
+        {rating && (
+          <div className="res-rating absolute-center">
+            {rating}
+            <i className="fi fi-rr-star absolute-center" />
+          </div>
+        )}
+      </div>
+      <div className="res-row">
+      {approxPrice && <div className="res-price">{approxPrice}</div>}
       </div>
     </div>
   );
